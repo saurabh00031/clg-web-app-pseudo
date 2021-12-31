656e635ad70b813e85989204da7ac88ac0bb32ed
Saurabh <patilsaurabh7777777@gmail.com>
2021-12-31 12:28:32 +0530

    adding files to repo

---
+++ ss.js
@@ -0,0 +1,19 @@
+
+
+
+
+function showMenu()
+{
+let fix=document.getElementById('nav-id');
+fix.style.right="0";
+}
+
+
+function hideMenu()
+{
+let fix=document.getElementById('nav-id');
+fix.style.right="-200px";
+
+
+}
+
